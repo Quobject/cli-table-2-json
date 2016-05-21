13c3bf391f807dc34e337b4558c915ec7e1a4656
MatthiasL <matthias_ludwig@yahoo.com>
2016-05-22 06:39:20 +1000

    1.0.7 new tslint.json

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,8 @@
-import test = require('blue-tape')
-import {  cliTable2Json } from './index'
+/* tslint:disable:no-shadowed-variable */
+import test = require('blue-tape');
+import {  cliTable2Json } from './index';
 
-const pkg = require('../package.json')
+// const pkg = require('../package.json');
 
 test('cli-table-2-json', t => {
   t.test('convert array of strings to array of objects', t => {
@@ -10,20 +11,40 @@ test('cli-table-2-json', t => {
       'consul1   -        amazonec2   Running   tcp://54.175.200.212:2376   ',
       'consul2   -        amazonec2   Running   tcp://52.23.236.38:2376     ',
       'consul3   -        amazonec2   Running   tcp://54.85.111.241:2376    ',
-      '']
+      ''];
 
 
-    let expected = [{ active: '-', driver: 'amazonec2', name: 'consul1', state: 'Running', swarm: '', url: 'tcp://54.175.200.212:2376' }, { active: '-', driver: 'amazonec2', name: 'consul2', state: 'Running', swarm: '', url: 'tcp://52.23.236.38:2376' }, { active: '-', driver: 'amazonec2', name: 'consul3', state: 'Running', swarm: '', url: 'tcp://54.85.111.241:2376' }]
+    let expected = [{
+      active: '-', driver: 'amazonec2',
+      name: 'consul1',
+      state: 'Running',
+      swarm: '',
+      url: 'tcp://54.175.200.212:2376',
+    }, {
+        active: '-',
+        driver: 'amazonec2',
+        name: 'consul2',
+        state: 'Running',
+        swarm: '',
+        url: 'tcp://52.23.236.38:2376',
+      }, {
+        active: '-',
+        driver: 'amazonec2',
+        name: 'consul3',
+        state: 'Running',
+        swarm: '',
+        url: 'tcp://54.85.111.241:2376',
+      }];
 
-    let result = cliTable2Json(lines)
+    let result = cliTable2Json(lines);
 
-    //console.log('result', result)
-    //console.log('expected', expected)
+    // console.log('result', result);
+    // console.log('expected', expected);
 
-    t.equal(result.length, expected.length)
-    t.equal(result[0].name, expected[0].name)
+    t.equal(result.length, expected.length);
+    t.equal(result[0].name, expected[0].name);
 
 
-    t.end()
-  })
-})
+    t.end();
+  });
+});
